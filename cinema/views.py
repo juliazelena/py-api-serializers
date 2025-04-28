79eb66d76333a6827b6a7d2d9ee4bac398df9fb7
ulichka <ulichka7615@gmail.com>
2025-04-28 16:55:17 +0200

    add attributes to select_related, add a serializer field for property

--- cinema/views.py
+++ cinema/views.py
@@ -58,5 +58,5 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = self.queryset
         if self.action in ("list", "retrieve"):
-            return queryset.select_related()
+            return queryset.select_related("movie", "cinema_hall")
         return queryset
